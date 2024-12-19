c3b7d167e4917052a43c431d4df30f1006f8f4e8
Garik Kamalyan <garsom@mail.ru>
2024-12-20 01:24:47 +0300

    2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/miniApp/', // Укажите путь, соответствующий названию вашего репозитория
   plugins: [react()],
-})
+});
