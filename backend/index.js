cf8c0e7bd0a9dc8248ace22a838fb35fb95193bc
Garik Kamalyan <garsom@mail.ru>
2024-12-26 18:34:18 +0300

    ё12

--- backend/index.js
+++ backend/index.js
@@ -16,4 +16,5 @@ app.use("/api", routes);
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Сервер запущен на http://localhost:${PORT}`);
+  console.log(`PORT из Railway: ${process.env.PORT}`);
 });
