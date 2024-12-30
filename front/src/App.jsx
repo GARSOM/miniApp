34710fff0949166613065775403c757f95a082cd
Garik Kamalyan <garsom@mail.ru>
2024-12-30 20:10:52 +0300

    das

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -26,19 +26,6 @@ const App = () => {
   const [companyInfo, setCompanyInfo] = useState(null);
   const [playerResources, setPlayerResources] = useState(null);
 
-
-
-  const indicators = playerResources ? [
-    // { icon: wheel, name: "Логистика", value: playerResources.logistics, description: "Влияет на время продажи товара" },
-    { icon: money, name: "Деньги", value: playerResources.money, description: "Ресурс для покупки и использования других ресурсов" },
-    // { icon: material, name: "Материалы", value: playerResources.material, description: "Необходим для производства товара" },
-    // { icon: facture, name: "Ленты конвеера", value: playerResources.production_lines, description: "Определяет количество одновременно производимых товаров" },
-    // { icon: energy, name: "Энергия", value: `${playerResources.current_energy} / ${playerResources.total_energy}`, description: "Необходима для запуска производства товара" },
-    // { icon: sklad, name: "Склад", value: `${playerResources.occupied_storage} / ${playerResources.total_storage}`, description: "Определяет максимальное количество производства товаров" },
-    // { icon: infrastucture, name: "Производство", value: playerResources.production, description: "Влияет на время производства товара" },
-    // { icon: car, name: "Транспорт ", value: playerResources.transport, description: "Определяет максимальное количество товаров на продажу" },
-  ] : [];
-
   useEffect(() => {
     const tg = window.Telegram?.WebApp?.initDataUnsafe?.user;
 
@@ -51,6 +38,7 @@ const App = () => {
           setIsRegistered(registered);
 
           if (registered) {
+            console.log(playerResources.money)
             const companyData = await getCompanyInfo(tg.id);
             setCompanyInfo(companyData);
 
@@ -75,7 +63,16 @@ const App = () => {
       setLoading(false);
     }
   }, []);
-
+  const indicators = playerResources ? [
+    // { icon: wheel, name: "Логистика", value: playerResources.logistics, description: "Влияет на время продажи товара" },
+    { icon: money, name: "Деньги", value: playerResources.money, description: "Ресурс для покупки и использования других ресурсов" },
+    // { icon: material, name: "Материалы", value: playerResources.material, description: "Необходим для производства товара" },
+    // { icon: facture, name: "Ленты конвеера", value: playerResources.production_lines, description: "Определяет количество одновременно производимых товаров" },
+    // { icon: energy, name: "Энергия", value: `${playerResources.current_energy} / ${playerResources.total_energy}`, description: "Необходима для запуска производства товара" },
+    // { icon: sklad, name: "Склад", value: `${playerResources.occupied_storage} / ${playerResources.total_storage}`, description: "Определяет максимальное количество производства товаров" },
+    // { icon: infrastucture, name: "Производство", value: playerResources.production, description: "Влияет на время производства товара" },
+    // { icon: car, name: "Транспорт ", value: playerResources.transport, description: "Определяет максимальное количество товаров на продажу" },
+  ] : [];
   const handleRegister = async (registrationData) => {
     try {
       const response = await registerUser(registrationData);
